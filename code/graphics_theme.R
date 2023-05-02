c5ffb03363d36d82245d73d58f73e83bee40a201
Ben Cresswell <benjamin.cresswell@jcu.edu.au>
2023-05-02 15:16:35 +1000

    Added graphics theme

---
+++ code/graphics_theme.R
@@ -0,0 +1,14 @@
+
+theme_csmp <- 
+theme_classic() +
+  theme(legend.position = "none")+
+  #theme(axis.text.x = element_blank()) +
+  theme(axis.title.x = element_text(family = "Arial", size=10, colour = 'black')) +
+  theme(axis.title.y = element_text(family = "Arial", size=10, colour = 'black')) +
+  theme(axis.text.x = element_text(family = "Arial", size= 10, colour = 'black')) +
+  theme(axis.text.y = element_text(family = "Arial", size= 10, colour = 'black')) +
+  #theme(axis.line = element_blank(), panel.border = element_rect(linewidth = 1, fill = "transparent"))
+  #theme(axis.line.y = element_blank()) +
+  theme(panel.border = element_blank())
+
+
