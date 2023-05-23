c00bb40c6705f87680cf0a238accc31df95d3777
Ben Cresswell <benjamin.cresswell@jcu.edu.au>
2023-05-23 12:32:50 +1000

    Folder restx

--- code/eda_shark_td.R
+++ code/eda_shark_td.R
@@ -1,21 +1,30 @@
-## ----include=FALSE------------------------------------------------------------
+## ----include=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE)
-source("packages.R")
-rm(list=ls()) # Clear out environment if necessary
+getwd()
+source("../packages.R")
+#rm(list=ls()) # Clear out environment if necessary
 
 
-## -----------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #load(file = "../data/Rdata/shark_daily_temps.Rdata")
 #load(file = "../data/Rdata/shark_daily_depths.Rdata")
 #load(file = "../data/Rdata/shark_tp_data.Rdata")
 
 
-## ----echo=FALSE, message=FALSE------------------------------------------------
-source(knitr::purl("../code/eda_shark_depth.Rmd", quiet=TRUE))
-source(knitr::purl("../code/eda_shark_temp.Rmd", quiet=TRUE))
+## ----echo=FALSE, message=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------
+source(knitr::purl("eda_shark_depth.Rmd", quiet=TRUE))
 
 
-## -----------------------------------------------------------------------------
+## ----echo=FALSE, message=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------
+source(knitr::purl("eda_shark_temp.Rmd", quiet=TRUE))
+
+
+## ----echo=FALSE, message=FALSE, warning=FALSE, fig.width=16, fig.height=6------------------------------------------------------------------------------------------------------
+setwd("../")
+source("00_wrangle/wrangling_SSTdata.R")
+
+
+## ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 shark_tp_data %>% 
   #filter(transmitter_id == "A69-9004-14037") %>%  # If you want to look at one individual
   ggplot() +
@@ -28,28 +37,55 @@ shark_tp_data %>%
   geom_point(aes(x = detection_timestamp, y = Temp)) 
 
 
-## -----------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 daily_tdplot <- shark_daily_depth_plot/shark_daily_temps_plot
 daily_tdplot
 
 
-## -----------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #ggsave(filename = "../output/tdplot.png", plot = tdplot, width = 16, height = 10, units = "cm")
 
 
-## -----------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 shark_daily_td <- 
-  full_join(shark_daily_depths, shark_daily_temps)
+  SST %>% 
+  full_join(shark_daily_depths) %>% 
+  full_join(shark_daily_temps)
 
 
-## -----------------------------------------------------------------------------
-shark_td_plot <- 
+## ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+shark_td_lm_plot <- 
 shark_daily_td %>% 
   ggplot() +
-  geom_point(aes(x = daily_max_temp, y = daily_max_depth)) +
+  geom_point(aes(x = SST, y = daily_max_depth)) +
   #geom_smooth(aes(x = daily_max_temp, y = daily_max_depth), method = "gam", formula = y ~ s(x, bs = 'cs')) +
-  geom_smooth(aes(x = daily_max_temp, y = daily_max_depth), method = "lm") +
+  geom_smooth(aes(x = SST, y = daily_max_depth), method = "lm") +
   theme_minimal() +
-  xlab("Daily maximum temp (°C)") + 
-  ylab("Daily maximum depth (m below sea surface)")
+  ylab("Daily maximum depth (mbsl)") + 
+  xlab("SST")
+shark_td_lm_plot
+
+
+## ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+shark_individual_daily_td <- 
+ shark_individual_daily_depths %>% 
+  full_join(shark_individual_daily_temps)
+
+
+## ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+shark_td_lm_individ_plot <- 
+shark_individual_daily_td %>% 
+  ggplot() +
+  geom_point(aes(x = SST, y = daily_max_depth)) +
+  #geom_smooth(aes(x = daily_max_temp, y = daily_max_depth), method = "gam", formula = y ~ s(x, bs = 'cs')) +
+  geom_smooth(aes(x = SST, y = daily_max_depth), method = "lm") +
+  theme_minimal() +
+  ylab("Daily maximum depth (mbsl)") + 
+  xlab("SST") +
+  facet_wrap(facets = "transmitter_id")
+shark_td_lm_individ_plot
+
+
+## ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# END
 
